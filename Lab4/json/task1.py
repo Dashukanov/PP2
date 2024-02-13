14504621d80e2bf338a3735e4034a528959cf2b2
Dashukanov <dashukanov2002@gmail.com>
2024-02-13 12:32:22 +0600

    json

--- Lab4/json/task1.py
+++ Lab4/json/task1.py
@@ -1,7 +1,7 @@
 import json
 
 def main():
-    with open("sample-data.json", "r") as file:
+    with open("C:\\Users\\admin\\Desktop\\repos\\PP2\\PP2-3\\Lab4\\json\\sample-data.json", "r") as file:
         data = json.load(file)
 
     print("Interface Status")
@@ -10,11 +10,13 @@ def main():
     print("-" * 80)
 
     for entry in data["imdata"]:
-        dn = entry["fvCEp"]["attributes"]["dn"]
-        description = entry["fvCEp"]["attributes"].get("descr", "")
-        speed = entry["fvCEp"]["attributes"].get("speed", "inherit")
-        mtu = entry["fvCEp"]["attributes"].get("mtu", "")
+        dn = entry["l1PhysIf"]["attributes"]["dn"]
+        description = entry["l1PhysIf"]["attributes"].get("descr", "")
+        speed = entry["l1PhysIf"]["attributes"].get("speed", "inherit")
+        mtu = entry["l1PhysIf"]["attributes"].get("mtu", "")
         print("{:<50}{:<20}{:<8}{:<8}".format(dn, description, speed, mtu))
 
 if __name__ == "__main__":
     main()
+
+
